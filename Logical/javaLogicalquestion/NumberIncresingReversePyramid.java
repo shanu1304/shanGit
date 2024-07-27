e17588e5765ee07281bb5043ff1faa3621d2ded1
Shantanu <shan1304.ds@gmail.com>
2024-07-27 10:32:13 +0530

    pattern example

--- Logical/javaLogicalquestion/NumberIncresingReversePyramid.java
+++ Logical/javaLogicalquestion/NumberIncresingReversePyramid.java
@@ -1,20 +1,19 @@
- // Number triangle pattern
+// Number-increasing reverse pyramid
 
-public class numberTrianglePattern {
+public class NumberIncresingReversePyramid {
     // Function to demonstrate pattern
     public static void printPattern(int n)
     {
         int i, j;
         // outer loop to handle number of rows
-        for (i = 1; i <= n; i++) {
-            // inner loop to print space
-            for (j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
-            // inner loop to print number
+        for (i = n; i >= 1; i--) {
+            // inner loop to handle number of columns
             for (j = 1; j <= i; j++) {
-                System.out.print(i + " ");
+                // printing column values upto the row
+                // value.
+                System.out.print(j + " ");
             }
+
             // print new line for each row
             System.out.println();
         }
--- Logical/javaLogicalquestion/squareHollowPartten.java
+++ Logical/javaLogicalquestion/squareHollowPartten.java
@@ -11,9 +11,11 @@ public class squareHollowPartten{
         for (i = 0; i < n; i++) {
             //  inner loop to handle number of columns
             for (j = 0; j < n; j++) {
-                // star will print only when  it is in first
-                // row or last row or first column or last
-                // column
+                /*
+                star will print only when  it is in first
+                 row or last row or first column or last
+                 column
+                 */
                 if (i == 0 || j == 0 || i == n - 1
                     || j == n - 1) {
                     System.out.print("*");
